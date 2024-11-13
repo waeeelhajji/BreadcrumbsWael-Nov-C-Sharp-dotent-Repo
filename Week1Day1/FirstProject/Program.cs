8a20bbcc5ec9f57650dacabc8e30336861aa4de4
waeeelhajji <waelhajji.contact@gmail.com>
2024-11-13 08:01:15 +0100

    Week1Day1

--- Week1Day1/FirstProject/Program.cs
+++ Week1Day1/FirstProject/Program.cs
@@ -55,18 +55,18 @@ Console.WriteLine(numberList.Count);
 
 //? Conditions
 
-// int temperature = 68;
-// if(temperature >= 72)
-// {
-//     // This executes if the temperature is greater than or equal to 72
-//     Console.WriteLine("It's a beautiful day to go outside!");
-// } else if(temperature > 64) {
-//     // This executes only if the temperature was NOT greater than or equal to 72 and IS above 64
-//     Console.WriteLine("I think it should be fine to go outside today with a light jacket.");    
-// } else {
-//     // If neither of the above conditions are met, we run the else statement
-//     Console.WriteLine("Maybe I'll stay inside today.");
-// }
+int temperature = 68;
+if(temperature >= 72)
+{
+    // This executes if the temperature is greater than or equal to 72
+    Console.WriteLine("It's a beautiful day to go outside!");
+} else if(temperature > 64) {
+    // This executes only if the temperature was NOT greater than or equal to 72 and IS above 64
+    Console.WriteLine("I think it should be fine to go outside today with a light jacket.");    
+} else {
+    // If neither of the above conditions are met, we run the else statement
+    Console.WriteLine("Maybe I'll stay inside today.");
+}
 
 //? Loops 
 
